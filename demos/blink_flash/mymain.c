205c00cc4e1eda0d6ab8b2d8073f3a79d01a7a07
descent <descent@weiya.com.tw>
2013-03-19 09:38:26 +0800

    modified:   mymain.c new file:   stm32f4xx.h new file:   stm32f4xx_gpio.h     stm32f4 led blink.

--- demos/blink_flash/mymain.c
+++ demos/blink_flash/mymain.c
@@ -1,8 +1,10 @@
 #define USE_STDPERIPH_DRIVER
 #include "stm32.h"
-#include "stm32f10x.h"
-#include "stm32_p103.h"
-#include "stm32f10x_rcc.h"
+#include "stm32f4xx_gpio.h"
+//#include "stm32f10x.h"
+//#include "stm32_p103.h"
+//#include "stm32f10x_rcc.h"
+#include "stm32f4xx.h"
 
 /*!< Uncomment the following line if you need to relocate your vector Table in
      Internal SRAM. */
@@ -12,10 +14,11 @@
 
 
 
-void busyLoop(uint32_t delay )
+void Delay(uint32_t delay )
 {
   while(delay) delay--;
 }
+#if 0
 
 void RCC_APB2PeriphClockCmd(uint32_t RCC_APB2Periph, FunctionalState NewState)
 {
@@ -137,7 +140,6 @@ void GPIO_WriteBit(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, BitAction BitVal)
   }
 }  
 
-#if 0
 static void SetSysClockToHSE(void)
 {
   __IO uint32_t StartUpCounter = 0, HSEStatus = 0;
@@ -293,13 +295,152 @@ void SystemInit (void)
 }
 #endif
 
+
+void RCC_AHB1PeriphClockCmd(uint32_t RCC_AHB1Periph, FunctionalState NewState)
+{ 
+  /* Check the parameters */
+  //assert_param(IS_RCC_AHB1_CLOCK_PERIPH(RCC_AHB1Periph));
+  
+  //assert_param(IS_FUNCTIONAL_STATE(NewState));
+  if (NewState != DISABLE)
+  {
+    RCC->AHB1ENR |= RCC_AHB1Periph;
+  } 
+  else
+  { 
+    RCC->AHB1ENR &= ~RCC_AHB1Periph;
+  }
+}  
+
+#define RCC_AHB1Periph_GPIOD             ((uint32_t)0x00000008)
+
+
+void GPIO_Init(GPIO_TypeDef* GPIOx, GPIO_InitTypeDef* GPIO_InitStruct)
+{
+  uint32_t pinpos = 0x00, pos = 0x00 , currentpin = 0x00;
+
+#if 0
+  /* Check the parameters */
+  assert_param(IS_GPIO_ALL_PERIPH(GPIOx));
+  assert_param(IS_GPIO_PIN(GPIO_InitStruct->GPIO_Pin));
+  assert_param(IS_GPIO_MODE(GPIO_InitStruct->GPIO_Mode));
+  assert_param(IS_GPIO_PUPD(GPIO_InitStruct->GPIO_PuPd));
+#endif
+  /* -------------------------Configure the port pins---------------- */
+  /*-- GPIO Mode Configuration --*/
+  for (pinpos = 0x00; pinpos < 0x10; pinpos++)
+  {
+    pos = ((uint32_t)0x01) << pinpos;
+    /* Get the port pins position */
+    currentpin = (GPIO_InitStruct->GPIO_Pin) & pos;
+
+    if (currentpin == pos)
+    {
+      GPIOx->MODER  &= ~(GPIO_MODER_MODER0 << (pinpos * 2));
+      GPIOx->MODER |= (((uint32_t)GPIO_InitStruct->GPIO_Mode) << (pinpos * 2));
+
+      if ((GPIO_InitStruct->GPIO_Mode == GPIO_Mode_OUT) || (GPIO_InitStruct->GPIO_Mode == GPIO_Mode_AF))
+      {
+        /* Check Speed mode parameters */
+        //assert_param(IS_GPIO_SPEED(GPIO_InitStruct->GPIO_Speed));
+
+        /* Speed mode configuration */
+        GPIOx->OSPEEDR &= ~(GPIO_OSPEEDER_OSPEEDR0 << (pinpos * 2));
+        GPIOx->OSPEEDR |= ((uint32_t)(GPIO_InitStruct->GPIO_Speed) << (pinpos * 2));
+
+        /* Check Output mode parameters */
+        //assert_param(IS_GPIO_OTYPE(GPIO_InitStruct->GPIO_OType));
+
+        /* Output mode configuration*/
+        GPIOx->OTYPER  &= ~((GPIO_OTYPER_OT_0) << ((uint16_t)pinpos)) ;
+        GPIOx->OTYPER |= (uint16_t)(((uint16_t)GPIO_InitStruct->GPIO_OType) << ((uint16_t)pinpos));
+      }
+
+      /* Pull-up Pull down resistor configuration*/
+      GPIOx->PUPDR &= ~(GPIO_PUPDR_PUPDR0 << ((uint16_t)pinpos * 2));
+      GPIOx->PUPDR |= (((uint32_t)GPIO_InitStruct->GPIO_PuPd) << (pinpos * 2));
+    }
+  }
+}
+
+void GPIO_SetBits(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+{   
+  /* Check the parameters */
+  //assert_param(IS_GPIO_ALL_PERIPH(GPIOx));
+  //assert_param(IS_GPIO_PIN(GPIO_Pin));
+    
+  GPIOx->BSRRL = GPIO_Pin;
+} 
+
+
+void GPIO_ResetBits(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+{
+  /* Check the parameters */
+  //assert_param(IS_GPIO_ALL_PERIPH(GPIOx));
+  //assert_param(IS_GPIO_PIN(GPIO_Pin));
+
+  GPIOx->BSRRH = GPIO_Pin;
+}
+
+
+
+
 int main(void)
 {
     //init_led();
     GPIO_InitTypeDef GPIO_InitStructure;
     
+    #if 0
     /* Enable GPIO C clock. */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    #endif
+
+    /* GPIOD Periph clock enable */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+  /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+      
+  while (1)
+  {     
+    /* PD12 to be toggled */
+    GPIO_SetBits(GPIOD, GPIO_Pin_12);
+        
+    /* Insert delay */ 
+    Delay(0x3FFFFF);
+
+    /* PD13 to be toggled */
+    GPIO_SetBits(GPIOD, GPIO_Pin_13);
+
+    /* Insert delay */
+    Delay(0x3FFFFF);
+        
+    /* PD14 to be toggled */
+    GPIO_SetBits(GPIOD, GPIO_Pin_14);
+      
+    /* Insert delay */
+    Delay(0x3FFFFF);
+    
+    /* PD15 to be toggled */
+    GPIO_SetBits(GPIOD, GPIO_Pin_15);
+
+    /* Insert delay */
+    Delay(0x7FFFFF);
+
+    GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+
+    /* Insert delay */
+    Delay(0xFFFFFF);
+  }
+
+
+#if 0
        
     /* Set the LED pin state such that the LED is off.  The LED is connected
      * between power and the microcontroller pin, which makes it turn on when
@@ -320,4 +461,5 @@ int main(void)
        GPIOC->BSRR = 0x00001000;
        busyLoop(500000);
     }
+#endif
 }
