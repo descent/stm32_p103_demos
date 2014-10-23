2a788f8165959da6081c05122fa117f50f6a20f3
descent <descent0@gmail.com>
2014-10-23 17:10:47 +0800

    call repl

--- demos/uart_echo/mymain.cpp
+++ demos/uart_echo/mymain.cpp
@@ -1,7 +1,10 @@
 #define USE_STDPERIPH_DRIVER
 #include "stm32f10x.h"
 #include "stm32_p103.h"
+#include "k_stdio.h"
+#include "s_eval.h"
 
+#if 0
 void send_byte(uint8_t b)
 {
     /* Send one byte */
@@ -10,15 +13,17 @@ void send_byte(uint8_t b)
     /* Loop until USART2 DR register is empty */
     while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
 }
+#endif
 
 extern "C"
 {
 int main(void)
 {
-    uint8_t b;
-
     init_rs232();
     USART_Cmd(USART2, ENABLE);
+#if 0
+    uint8_t b;
+
 
     while(1) {
         /* Loop until the USART2 has received a byte. */
@@ -33,5 +38,20 @@ int main(void)
         send_byte(b);
         send_byte('\n');
     }
+#endif
+  init_eval();
+  Environment *global_env = get_env(0, "global");
+  create_primitive_procedure(global_env);
+#if 0
+  while(1)
+  {
+    myprint("simple scheme> ");
+    getchar();
+  }
+#else
+  repl("simple scheme> ", global_env);
+#endif
+  return 0;
+
 }
 }
