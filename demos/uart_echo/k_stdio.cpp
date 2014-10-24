301b52b1d7c0aedbbe110a6525143e4501448cfd
descent <descent0@gmail.com>
2014-10-24 15:47:09 +0800

    add:   void myprint(int num) fixed:   int ungetch(int c) modify:   int getchar(): if press enter print \r\n to change new line.

--- demos/uart_echo/k_stdio.cpp
+++ demos/uart_echo/k_stdio.cpp
@@ -23,23 +23,59 @@ void myprint(const char *str)
     send_byte(*p++);
 }
 
+void myprint(int num)
+{
+  char str[10];
+  s32_itoa_s(num, str, 10);
+  myprint(str);
+}
+
 int keep_char = -1;
 
 int ungetch(int c)
 {
+#if 0
+  myprint("\r\nun: ");
+  myprint(c);
+  myprint("\r\n");
+#endif
   keep_char = c;
   return 0;
 }
 
 int getchar()
 {
+  int b;
+
   if (keep_char != -1 )
-    return keep_char;
+  {
+    b = keep_char;
+    keep_char = -1;
+    return b;
+  }
   else
   {
-    int b;
     while(USART_GetFlagStatus(USART2, USART_FLAG_RXNE) == RESET);
     b = (USART_ReceiveData(USART2) & 0x7F);
-    send_byte(b);
+    switch (b)
+    {
+      case '\r': // press enter
+      {
+        myprint("\r\n");
+        break;
+      }
+#if 0
+      case '\n':
+      {
+        break;
+      }
+#endif
+      default:
+      {
+        send_byte(b);
+        break;
+      }
+    }
+    return b;
   }
 }
--- demos/uart_echo/k_stdio.h
+++ demos/uart_echo/k_stdio.h
@@ -4,6 +4,7 @@
 #include "type.h"
 
 void myprint(const char *str);
+void myprint(int num);
 void send_byte(u8 b);
 int ungetch(int c);
 int getchar();
