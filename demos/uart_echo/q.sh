e027f216665c155ffc8a75a9e05cba937ab7abb5
descent <descent0@gmail.com>
2014-10-24 15:49:20 +0800

    use new version qmeu stm32

--- demos/uart_echo/q.sh
+++ demos/uart_echo/q.sh
@@ -1,2 +1,6 @@
 #/home/descent/git/jserv-course/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin
-/home/descent/git/jserv-course/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin -S -gdb tcp::1234
+#/home/descent/git/jserv-course/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin -S -gdb tcp::1234
+
+/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin  
+#/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin  -S -gdb tcp::1234
+
