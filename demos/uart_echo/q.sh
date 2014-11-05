0b37dce94490059ffb00e84d72810d6eae43a68f
descent <descent0@gmail.com>
2014-11-05 18:02:24 +0800

    add more arguments

--- demos/uart_echo/q.sh
+++ demos/uart_echo/q.sh
@@ -1,6 +1,11 @@
 #/home/descent/git/jserv-course/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin
 #/home/descent/git/jserv-course/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin -S -gdb tcp::1234
 
-/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin  
+/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin -display sdl
 #/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin  -S -gdb tcp::1234
+
+
+ #/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin  -nographic
+ #/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin  -curses
+
 
