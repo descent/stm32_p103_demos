3ecbb2d396a83e0c5b5f62b68e9606dce76e56b1
descent <descent0@gmail.com>
2014-11-06 18:10:14 +0800

    add float_to_str()

--- demos/uart_echo/k_string.h
+++ demos/uart_echo/k_string.h
@@ -214,5 +214,78 @@ static inline bool isdigit(int c)
     return false;
 }
 
+static inline int mypow(int x, int y)
+{
+  int r=1;
+  for (int i=0 ; i < y ; ++i)
+    r*=x;
+  return r;
+}
+
+const int PRECISION = 6;
+
+static inline u8* float_to_str(float num)
+{
+   int whole_part = num;
+   int digit = 0, reminder =0;
+   //int log_value = log10(num), 
+   int index;
+   float wt = 0.0;
+
+   // String containg result
+   static u8 str[30];
+
+   //Initilise stirng to zero
+   //memset(str, 0 ,20);
+   
+   //u8 whole_str[20] ;
+   s32_itoa(whole_part, str, 10);
+#if 0
+   //Extract the whole part from float num
+   for(int  i = 1 ; i < log_value + 2 ; i++)
+   {
+       wt  =  mypow(10,i);
+       reminder = whole_part  %  wt;
+       digit = (reminder - digit) / (wt/10);
+
+       //Store digit in string
+       str[index--] = digit + 48;              // ASCII value of digit  = digit + 48
+       if (index == -1)
+          break;    
+   }
+#endif
+    //index = log_value + 1;
+    index = s_strlen((const char*)str);
+    str[index] = '.';
+
+   float fraction_part  = num - whole_part;
+   float tmp1 = fraction_part,  tmp =0;
+
+#if 0
+   cout << "fraction_part: " << fraction_part << endl;
+   cout << "num: " << num << endl;
+   cout << "whole_part: " << whole_part << endl;
+#endif
+   //Extract the fraction part from  num
+   for( int i= 1; i < PRECISION; i++)
+   {
+      wt =10; 
+      tmp  = tmp1 * wt;
+      digit = tmp;
+      //cout << "digit: " << digit << endl;
+      //cout << "tmp: " << tmp << endl;
+      //cout << "tmp1: " << tmp1 << endl;
+
+      //Store digit in string
+      str[++index] = digit +48;           // ASCII value of digit  = digit + 48
+      tmp1 = tmp - digit;
+      if (tmp1 == 0.0)
+        break;
+   }    
+   str[index] = 0;
+
+   return str;
+}
+
 
 #endif
