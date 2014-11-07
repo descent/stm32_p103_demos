b46cfd64c3b03c85d03c9a5b378431dbb4edddbb
descent <descent0@gmail.com>
2014-11-07 23:56:22 +0800

    add more arugment

--- demos/uart_echo/q.sh
+++ demos/uart_echo/q.sh
@@ -1,8 +1,8 @@
-#/home/descent/git/jserv-course/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin
-#/home/descent/git/jserv-course/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin -S -gdb tcp::1234
+#/home/descent/git/jserv-course/qemu_stm32/arm-softmmu/qemu-system-arm -display sdl -M stm32-p103 -kernel mymain.bin
+#/home/descent/git/jserv-course/qemu_stm32/arm-softmmu/qemu-system-arm -display sdl -M stm32-p103 -kernel mymain.bin -S -gdb tcp::1234
 
-/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin -display sdl
-#/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin  -S -gdb tcp::1234
+/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -display sdl -M stm32-p103 -kernel mymain.bin -display sdl
+#/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -display sdl -M stm32-p103 -kernel mymain.bin  -S -gdb tcp::1234
 
 
  #/media/2/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel mymain.bin  -nographic
