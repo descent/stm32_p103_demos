0ac976a39b841b8586691f8069503a7f2aa9eb3a
descent <descent0@gmail.com>
2014-11-15 20:52:19 +0800

    init all deque

--- demos/uart_echo/mymain.cpp
+++ demos/uart_echo/mymain.cpp
@@ -3,6 +3,10 @@
 #include "stm32_p103.h"
 #include "k_stdio.h"
 #include "s_eval.h"
+#include "gdeque.h"
+#include "cstring.h"
+
+using namespace DS;
 
 #if 0
 void send_byte(uint8_t b)
@@ -15,23 +19,107 @@ void send_byte(uint8_t b)
 }
 #endif
 
-void abc(const char *str)
+uint8_t get_byte()
 {
-  myprint(str);
-  getchar();
+  while(USART_GetFlagStatus(USART2, USART_FLAG_RXNE) == RESET);
+
+  return (USART_ReceiveData(USART2) & 0x7F);
 }
+
+u8 buf[20];
+
+
+bool is_up(const char *buf)
+{
+  if (buf[0] == 27 && buf[1] == '[' && buf[2] == 'A')
+    return true;
+  else
+    return false;
+}
+
+bool is_down(const char *buf)
+{
+  if (buf[0] == 27 && buf[1] == '[' && buf[2] == 'B')
+    return true;
+  else
+    return false;
+}
+
+Deque<DS::CString> deque;
+DS::Deque<int> line_buf;
 
 extern "C"
 {
 int main(void)
 {
-    init_rs232();
-    USART_Cmd(USART2, ENABLE);
-  mydeque.init();
+  init_rs232();
+  USART_Cmd(USART2, ENABLE);
+
+#if 0
+
+
+  u8 ch;
+  int i=0;
+  char input_str[20];
+  char *sb=0;
+  DS::CString ps;
+  int index = 0;
+  while(1)
+  {
+    sb = gets(input_str, 20);
+    if (is_up(sb))
+    {
+      ++index;
+      if (deque.back(index, ps) == true)
+      {
+        myprint(ps.c_str());
+      }
+      else
+        --index;
+    }
+    else if (is_down(sb))
+         {
+            --index;
+            if (deque.back(index, ps) == true)
+            {
+              myprint(ps.c_str());
+            }
+            else
+            {
+              ++index;
+            }
+
+         }
+         else
+         {
+           myprint(sb);
+           myprint("\r\n");
+           ps = sb;
+           deque.push_back(ps);
+         }
+
+
+  }
+
+    float f=1.5;
+    myprint_float(f);
+    myprint("\r\nabc");
+    while(1);
+  int b;
+  while(1)
+  {
+    b=read_char();
+  }
+
+#else
   init_eval();
+  deque.init();
+  mydeque.init();
+  line_buf.init();
   Environment *global_env = get_env(0, "global");
   create_primitive_procedure(global_env);
-  repl("simple scheme> ", global_env);
+  non_os_repl("simple scheme> ", global_env);
+#endif
   return 0;
 
 }
