432fd89c86c7192e6478fbab62181ad2bbcf5955
descent <descent0@gmail.com>
2014-11-17 10:26:37 +0800

    new file:   ss.sh

---
+++ demos/uart_echo/ss.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+ln -s ~/git/simple_scheme/token_container.h .
+ln -s ~/git/simple_scheme/token_container.cpp .
+ln -s ~/git/simple_scheme/s_eval.h .
+ln -s ~/git/simple_scheme/s_eval.cpp  .
+ln -s ~/git/simple_scheme/cell.h .
+ln -s ~/git/simple_scheme/cell.cpp .
+ln -s ~/git/jserv-course/stm32f4_prog/stm32f4_simple_scheme/k_stdio.h .
+ln -s ~/git/jserv-course/stm32f4_prog/stm32f4_simple_scheme/k_stdio.cpp .
+ln -s ~/git/jserv-course/stm32f4_prog/stm32f4_simple_scheme/k_string.h .
+
+ln -s /home/descent/git/progs/queue/gdeque.h .
+ln -s /home/descent/git/progs/queue/gdeque.cpp .
+
+ln -s /home/descent/git/progs/queue/cstring.h .
+ln -s /home/descent/git/progs/queue/cstring.cpp .
+
+
